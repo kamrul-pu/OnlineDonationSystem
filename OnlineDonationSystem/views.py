1268e8e925db50c31ae61114b09853d98f2bc2e6
kamrul Hasan <kamrul.h456@gmail.com>
2022-03-14 00:05:21 +0600

    Image Folder Uploaded

--- OnlineDonationSystem/views.py
+++ OnlineDonationSystem/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def home(request):
     diction = {'title':'Homepage'}
-    return render(request,'Accounts/index.html',context=diction)
+    return render(request,'home.html',context=diction)
