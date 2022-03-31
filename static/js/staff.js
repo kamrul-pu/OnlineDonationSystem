ca7ea856ecb4d02f54a5682e7d1c45ef0a9bb5c7
kamrul Hasan <kamrul.h456@gmail.com>
2022-04-01 00:42:38 +0600

    Source code Fromated

---
+++ static/js/staff.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "300px";
+    document.getElementById("mySidepanel").style.height = "500px";
+}
+
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
